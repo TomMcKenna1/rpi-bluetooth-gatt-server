28c05c470eb443f32fff6b319e8ba06c491bfa16
Tom McKenna <tom2mckenna@gmail.com>
2024-02-12 13:49:06 +0000

    add chars

--- start.py
+++ start.py
@@ -3,9 +3,132 @@ from dbus_fast import BusType, PropertyAccess
 from dbus_fast.aio import MessageBus
 from dbus_fast.service import ServiceInterface, method, dbus_property
 
-class MarketMirrorAdvert(ServiceInterface):
+
+class MarketMonitorApplication(ServiceInterface):
     def __init__(self):
-        self.name = "Market Monitor"
+        self.services = []
+        super().__init__("marketmonitorapp")
+
+    def add_service(self, service):
+        self.services.append(service)
+
+    @method()
+    def GetManagedObjects(self) -> "a{oa{sa{sv}}}":
+        response = {}
+
+        for service in self.services:
+            response[service.get_path()] = service.get_properties()
+            chrcs = service.get_chars()
+            for chrc in chrcs:
+                response[chrc.get_path()] = chrc.get_properties()
+
+        return response
+
+
+class MarketMonitorService(ServiceInterface):
+    def __init__(self, uuid, path):
+        self.path = path
+        self.name = "marketmonitorservice"
+        self.uuid = uuid
+        self.primary = True
+        self.chars = []
+        self.includes = []
+        super().__init__(self.name)
+
+    def get_path(self):
+        return self.path
+
+    def get_properties(self):
+        return {
+            "org.bluez.GattService1": {
+                "UUID": self.uuid,
+                "Primary": self.primary,
+                "Characteristics": self.includes,
+            }
+        }
+    
+    def get_chars(self):
+        return self.chars
+
+    def add_char(self, char):
+        self.chars.append(char)
+        self.includes.append(char.get_path())
+
+    @dbus_property(PropertyAccess.READ)
+    def UUID(self) -> "s":
+        return self.uuid
+
+    @dbus_property(PropertyAccess.READ)
+    def Primary(self) -> "b":
+        return self.primary
+
+    @dbus_property()
+    def Includes(self) -> "ao":
+        return self.includes
+
+    @Includes.setter()
+    def Includes(self, value: "ao"):
+        self.includes = value
+
+
+class MarketMonitorChar(ServiceInterface):
+    def __init__(self, path, uuid):
+        self.path = path
+        self.service = "/".join(path.split("/")[0:-1])
+        self.uuid = uuid
+        self.flags = ["read", "write"]
+        self.value = 1
+
+    def get_path(self):
+        return self.path
+
+    def get_properties(self):
+        return {
+            "org.bluez.GattCharacteristic1": {
+                "Service": self.service,
+                "UUID": self.uuid,
+                "Flags": self.flags,
+            }
+        }
+
+    @method()
+    def ReadValue(self, opt):
+        print("Read!")
+        return self.value
+
+    @method()
+    def WriteValue(self, val, opt):
+        self.value = val
+        print("Written!")
+
+    @method()
+    def StartNotify(self):
+        pass
+
+    @method()
+    def StopNotify(self):
+        pass
+
+    @dbus_property(PropertyAccess.READ)
+    def UUID(self) -> "s":
+        return self.uuid
+
+    @dbus_property(PropertyAccess.READ)
+    def Service(self) -> "s":
+        return self.service
+
+    @dbus_property(PropertyAccess.READ)
+    def Flags(self) -> "as":
+        return self.flags
+
+    @dbus_property(PropertyAccess.READ)
+    def MTU(self) -> "q":
+        return 10000
+
+
+class MarketMonitorAdvert(ServiceInterface):
+    def __init__(self):
+        self.name = "marketmonitor"
         self.device_name = self.name
         self.local_name = self.name
         self.type = "peripheral"
@@ -15,82 +138,111 @@ class MarketMirrorAdvert(ServiceInterface):
         self.service_data = {}
         self.data = {}
         self.discoverable = True
-        self.discoverable_timeout = 100
+        self.discoverable_timeout = 0
         self.includes = []
         super().__init__(self.name)
-        
+
     @method()
     def Release(self):
         print("Released service")
+
     @dbus_property(PropertyAccess.READ)
-    def Type(self) -> 's':
+    def Type(self) -> "s":
         return self.type
+
     @dbus_property()
-    def ServiceUUIDs(self) -> 'as':
+    def ServiceUUIDs(self) -> "as":
         return self.service_uuids
+
     @ServiceUUIDs.setter
-    def ServiceUUIDs(self, value: 'as'):
+    def ServiceUUIDs(self, value: "as"):
         self.service_uuids = value
+
     @dbus_property()
-    def ManufacturerData(self) -> 'a{sv}':
+    def ManufacturerData(self) -> "a{sv}":
         return self.manufacturer_data
+
     @ManufacturerData.setter
-    def ManufacturerData(self, value: 'a{sv}'):
+    def ManufacturerData(self, value: "a{sv}"):
         self.manufacturer_data = value
+
     @dbus_property()
-    def SolicitUUIDs(self) -> 'as':
+    def SolicitUUIDs(self) -> "as":
         return self.solicit_uuids
+
     @SolicitUUIDs.setter
-    def SolicitUUIDs(self, value: 'as'):
+    def SolicitUUIDs(self, value: "as"):
         self.solicit_uuids = value
+
     @dbus_property()
-    def ServiceData(self) -> 'a{sv}':
+    def ServiceData(self) -> "a{sv}":
         return self.service_data
+
     @ServiceData.setter
-    def ServiceData(self, value: 'a{sv}'):
+    def ServiceData(self, value: "a{sv}"):
         self.service_data = value
+
     @dbus_property()
-    def Data(self) -> 'a{sv}':
+    def Data(self) -> "a{sv}":
         return self.data
+
     @Data.setter
-    def Data(self, value: 'a{sv}'):
+    def Data(self, value: "a{sv}"):
         self.data = value
+
     @dbus_property()
-    def Discoverable(self) -> 'b':
+    def Discoverable(self) -> "b":
         return self.discoverable
+
     @Discoverable.setter
-    def Discoverable(self, value: 'b'):
+    def Discoverable(self, value: "b"):
         self.discoverable = value
+
     @dbus_property()
-    def DiscoverableTimeout(self) -> 'q':
+    def DiscoverableTimeout(self) -> "q":
         return self.discoverable_timeout
+
     @DiscoverableTimeout.setter
-    def DiscoverableTimeout(self, value: 'q'):
+    def DiscoverableTimeout(self, value: "q"):
         self.discoverable_timeout = value
+
     @dbus_property()
-    def Includes(self) -> 'as':
+    def Includes(self) -> "as":
         return self.includes
+
     @Includes.setter
-    def Includes(self, value: 'as'):
+    def Includes(self, value: "as"):
         self.includes = value
+
     @dbus_property()
-    def LocalName(self) -> 's':
+    def LocalName(self) -> "s":
         return self.local_name
+
     @LocalName.setter
-    def LocalName(self, value: 's'):
+    def LocalName(self, value: "s"):
         self.local_name = value
+
     @dbus_property()
-    def DeviceName(self) -> 's':
+    def DeviceName(self) -> "s":
         return self.device_name
+
     @DeviceName.setter
-    def DeviceName(self, value: 's'):
+    def DeviceName(self, value: "s"):
         self.device_name = value
 
 
 async def main():
     bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
-    advert = MarketMirrorAdvert()
-    bus.export('/org/bluez/example/advertisement', advert)
+    advert = MarketMonitorAdvert()
+    application = MarketMonitorApplication()
+    service = MarketMonitorService("0xFFFF", "/org/bluez/example/service")
+    char = MarketMonitorChar("0x1234", "/org/bluez/example/service/char1")
+    service.add_char(char)
+    application.add_service(service)
+    bus.export("/org/bluez/example/service/char1", char)
+    bus.export("/org/bluez/example/service", service)
+    bus.export("/", application)
+    bus.export("/org/bluez/example/advertisement", advert)
     intro = await bus.introspect("org.bluez", "/org/bluez/hci0")
     # print([[method.name for method in interface.methods] for interface in intro.interfaces])
     obj = bus.get_proxy_object("org.bluez", "/org/bluez/hci0", intro)
@@ -101,10 +253,11 @@ async def main():
     await adapter_proxy.set_pairable(True)
     before = await adv_man_proxy.get_active_instances()
     print(before)
-    awd = await adv_man_proxy.call_register_advertisement('/org/bluez/example/advertisement', {})
+    awd = await adv_man_proxy.call_register_advertisement(
+        "/org/bluez/example/advertisement", {}
+    )
     after = await adv_man_proxy.get_active_instances()
     print(after)
-    
 
 
 asyncio.run(main())
